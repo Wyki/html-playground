1a61784bfb4a3e95b93e143c76e75e3d473d405f
Wyki <morten.helwers@rwth-aachen.de>
2013-07-02 15:21:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,8 @@
 			echo '<p><a href="story.php?story='.$zeile['ID'].'&ID=0" class="btn btn-primary btn-large">Lesen &raquo;</a></p>';
 			echo '</div>';
 		}
+		
+		
 		?>
 		<script src="http://code.jquery.com/jquery.js"></script>
 		<script src="js/bootstrap.min.js"></script>
